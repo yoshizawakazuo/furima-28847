d065bf3e0f9c36a934ee3256c5226c83abad2ee4
yoshizawakazuo <69493556+yoshizawakazuo@users.noreply.github.com>
2020-09-17 18:10:13 +0900

    Besic認証実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :basic_auth
 
   protected
 
@@ -11,9 +11,9 @@ class ApplicationController < ActionController::Base
   private
 
   def basic_auth
-    authenticate_or_request_with_http_basic do |nickname, password|
-      nickname == ENV["BASIC_AUTH_USER"] && password == ENV["BASIC_AUTH_PASSWORD"] 
+    authenticate_or_request_with_http_basic do |username, password|
+      username == 'admin' && password == '2222'
     end
   end
-  
+
 end
